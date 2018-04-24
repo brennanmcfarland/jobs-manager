d61eb7998717885da091c4c6c1c8d541419fa427
Brennan McFarland <4brothersnosisters@gmail.com>
2018-04-23 20:37:31 -0400

    did stuf

--- jmmanager.py
+++ jmmanager.py
@@ -8,7 +8,7 @@ import time
 cores = ps.cpu_count()
 queueLock = Lock() # a semaphore for the lock queue
 queuedjobs = SortedListWithKey(key=lambda job: job.nice) # sorted by nice value
-runningjobs = [None for i in cores]
+runningjobs = [None]*cores
 newid = 0
 
 def add_job(command, parsed_command, priority):
@@ -21,12 +21,23 @@ def add_job(command, parsed_command, priority):
 
 # called every time quantum
 def manage_jobs():
-    time.sleep(.5)
-    for j in range(len(runningjobs)):
-        if runningjobs[j].nice == 0:
-            runningjobs[j] = start_next_job()
-        else:
-            runningjobs[j].nice -= 1
+    print("DSFLKJDS")
+    while True:
+        print("TESLKTJ")
+        time.sleep(.5)
+        for j in range(len(runningjobs)):
+            print(runningjobs[j])
+            if runningjobs[j] is None or runningjobs[j].nice == 0:
+                try:
+                    with (yield from queueLock):
+                        runningjobs[j] = queuedjobs.pop()
+                        runningjobs[j].start()
+                except:
+                    runningjobs[j] = None
+            else:
+                if runningjobs[j] is not None:
+                    runningjobs[j].nice -= 1
+        # TODO: add removed jobs back onto the queue with new nice value
 
 
 def start_next_job():
