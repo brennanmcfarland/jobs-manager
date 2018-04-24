0597e5c75b832d75937693832e5a5d11f46d9a78
Josh Reichman <reichmanjosh@gmail.com>
2018-04-23 20:03:15 -0400

    nice field, extracted out process creation code

--- jmjob.py
+++ jmjob.py
@@ -10,13 +10,18 @@ class JMJob:
     pid = ERROR_VAL
     command = ""
     subprocess = None
+    nice = ERROR_VAL
 
     def __init__(self, id, command, parsed_command, priority):
         print(command)
         self.command = command
         self.priority = priority
         self.id = id
-        self.subprocess = sp.Popen(parsed_command) # creates a new process
+        #self.nice = nice
+        self.start(parsed_command)
+
+    def start(self, parsed_command):
+        self.subprocess = sp.Popen(parsed_command)  # creates a new process
         self.pid = self.subprocess.pid
         self.status()
 
@@ -26,6 +31,9 @@ class JMJob:
 
     def is_terminated(self):
         return not self.subprocess.poll()
+
+    def calculate_nice(self):
+        return self.nice # TODO: Calculate nice value rather than simply return it
 
     def kill(self):
         self.subprocess.kill()
