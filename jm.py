0c27229197754d1725ea6467e9c6f4979431eb10
Brennan McFarland <4brothersnosisters@gmail.com>
2018-04-23 20:12:09 -0400

    jobs manager should now concurrently run processes

--- jm.py
+++ jm.py
@@ -1,4 +1,8 @@
+import jmmanager as manager
 from jmshell import ShellLoop
+from multiprocessing import Process
 
+manager_loop = Process(target=manager.manage_jobs)
+manager_loop.start()
 shell = ShellLoop()
 shell.cmdloop()
--- jmmanager.py
+++ jmmanager.py
@@ -1,12 +1,38 @@
 from jmjob import JMJob
 from sortedcontainers import SortedListWithKey
+import psutil as ps
+from asyncio.locks import Lock
+import time
 
 
-jobs = SortedListWithKey()
+cores = ps.cpu_count()
+queueLock = Lock() # a semaphore for the lock queue
+queuedjobs = SortedListWithKey(key=lambda job: job.nice) # sorted by nice value
+runningjobs = [None for i in cores]
 newid = 0
 
 def add_job(command, parsed_command, priority):
     global newid
     newjob = JMJob(newid, command, parsed_command, priority)
-    jobs.add(newjob)
+    with (yield from queueLock):
+        queuedjobs.add(newjob)
     newid += 1
+
+
+# called every time quantum
+def manage_jobs():
+    time.sleep(.5)
+    for j in range(len(runningjobs)):
+        if runningjobs[j].nice == 0:
+            runningjobs[j] = start_next_job()
+        else:
+            runningjobs[j].nice -= 1
+
+
+def start_next_job():
+    try:
+        with (yield from queueLock):
+            next_job = queuedjobs.pop()
+    except: return None
+    next_job.start()
+    return next_job
